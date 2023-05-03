63cbc0a217db46842904c7778751419b9e010178
Dan97O <dan97-oddo@live.it>
2023-05-03 16:26:55 +0200

    add markup script

--- script.php
+++ script.php
@@ -8,11 +8,49 @@ stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
 $name = $_GET["name"];
 $password = $_GET["censored"];
 $censored = str_replace($password,"***",$password);
-echo $name;
-echo strlen($name);
-
+//echo $name;
+//echo strlen($name);
 //echo $password;
-echo $censored;
+//echo $censored;
 //echo strlen($password);
-
 ?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+  <title>Document</title>
+</head>
+<body>
+
+  
+<div class="container">
+  <div class="row">
+    <div class="col-12 d-flex justify-content-center align-items-center mt-5 flex-column">
+    <h4>Paragrafo:</h4>
+    <div>
+      <p>
+      <?php echo $name;?> n: <?php echo strlen($name); ?>
+      </p>
+    </div>
+    <div>
+      <p>
+      <?php echo $name;?> n: <?php echo strlen($name); ?>
+      </p>
+    </div>
+    <h4>Password:</h4>
+    <div>
+      <p>
+      <?php echo $censored;?> 
+      </p>
+    </div>
+    </div>
+  </div>
+</div>
+
+
+</body>
+</html>
