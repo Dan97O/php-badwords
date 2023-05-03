68a0f9eaa0b65aee31092c15a9ac36b12ff19d16
Dan97O <dan97-oddo@live.it>
2023-05-03 14:57:45 +0200

    add markup index

--- index.php
+++ index.php
@@ -4,15 +4,37 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
   <title>Document</title>
 </head>
 <body>
-  
-
-<!-- 
+  <!-- 
   Creare un form PHP che permetta di inviare due campi: un paragrafo ed una parola da censurare.
 Gestire il tutto con due file diversi.
 Il primo file (index.php) dovrà permettere all’utente di inserire i dati (tramite un form) e inviare la richiesta al server.
  -->
+  
+
+<div class="container">
+  <div class="row">
+    <div class="col-12 d-flex justify-content-center mt-5">
+      <form action="script.php" method="get">
+        <div class="mb-3">
+          <label for="name">Paragrafo</label>
+          <input type="text" name="name" id="name" class="form-control" placeholder="Inserisci il paragrafo...">
+        </div>
+        <div class="mb-3">
+          <label for="censored" class="form-label">Parola da censurare</label>
+          <input type="text" name="censored" id="censored" class="form-control" placeholder="Inserisci la parola...">
+        </div>
+        <div class="mb-3">
+          <button type="submit" class="btn btn-primary">Invia</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+
 </body>
 </html>
